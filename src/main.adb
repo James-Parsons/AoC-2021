ec5703f3834e07f10a4f32e1a911e03b1a530b4d
James Parsons <jamesmp98@gmail.com>
2021-12-02 23:20:48 -0500

    Replaced absolute paths to the input with relative paths.

--- src/main.adb
+++ src/main.adb
@@ -1,4 +1,5 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Directories; use Ada.Directories;
 with Day_One_Part_One; use Day_One_Part_One;
 with Day_One_Part_Two; use Day_One_Part_Two;
 with Day_Two_Part_One; use Day_Two_Part_One;
@@ -6,9 +7,10 @@ with Day_Two_Part_Two; use Day_Two_Part_Two;
 
 procedure Main is
 begin
-   Day_One_Part_One.Run("/home/james/Desktop/AOC/input/input_1");
-   Day_One_Part_Two.Run("/home/james/Desktop/AOC/input/input_1");
+   Put_Line(Current_Directory);
+   Day_One_Part_One.Run("./input/input_1");
+   Day_One_Part_Two.Run("./input/input_1");
 
-   Day_Two_Part_One.Run("/home/james/Desktop/AOC/input/input_2");
-   Day_Two_Part_Two.Run("/home/james/Desktop/AOC/input/input_2");
+   Day_Two_Part_One.Run("./input/input_2");
+   Day_Two_Part_Two.Run("./input/input_2");
 end Main;
